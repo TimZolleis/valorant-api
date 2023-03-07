d40cc343030de829e9e0db5f7812a6140ba5458a
Tim <tim@zolleis.net>
2023-03-07 23:21:33 +0100

    change vercel dependency

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,33 +1,13 @@
-import { PassThrough } from 'stream';
-import { renderToPipeableStream } from 'react-dom/server';
+import handleRequest from '@vercel/remix-entry-server';
 import { RemixServer } from '@remix-run/react';
-import { Response } from '@remix-run/node'; // or cloudflare/deno
-import type { EntryContext, Headers } from '@remix-run/node'; // or cloudflare/deno
+import type { EntryContext } from '@remix-run/server-runtime';
 
-export default function handleRequest(
+export default function (
     request: Request,
     responseStatusCode: number,
     responseHeaders: Headers,
     remixContext: EntryContext
 ) {
-    return new Promise((resolve) => {
-        const { pipe } = renderToPipeableStream(
-            <RemixServer context={remixContext} url={request.url} />,
-            {
-                onShellReady() {
-                    const body = new PassThrough();
-
-                    responseHeaders.set('Content-Type', 'text/html');
-
-                    resolve(
-                        new Response(body, {
-                            status: responseStatusCode,
-                            headers: responseHeaders,
-                        })
-                    );
-                    pipe(body);
-                },
-            }
-        );
-    });
+    const remixServer = <RemixServer context={remixContext} url={request.url} />;
+    return handleRequest(request, responseStatusCode, responseHeaders, remixServer);
 }
