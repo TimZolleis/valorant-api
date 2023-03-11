4c5ff08e4a57129ed17f21f0de4498e85704880a
tim <tim@zolleis.net>
2023-03-11 20:46:35 +0100

    fix broken api

---
+++ app/routes/api/match/live.ts
@@ -0,0 +1,51 @@
+import type { DataFunctionArgs } from '@remix-run/node';
+import { json } from '@remix-run/node';
+import { requireUser } from '~/utils/session/session.server';
+import { getRunningCoregameMatch, getRunningPregameMatch } from '~/utils/match/livematch.server';
+import { NoPregameFoundException } from '~/exceptions/NoPregameFoundException';
+import { NoCoregameFoundException } from '~/exceptions/NoCoregameFoundException';
+import type { ValorantUser } from '~/models/user/ValorantUser';
+import { get } from '@vercel/edge-config';
+
+export type LiveMatchRoute = Awaited<LoaderData>;
+export type GameStatus = 'pregame' | 'coregame' | 'not in game';
+type LoaderData = {
+    status: GameStatus;
+};
+
+async function detectGame(user: ValorantUser, puuid: string) {
+    try {
+        const mockPregame = await get('mockPregame');
+        const pregame = mockPregame
+            ? true
+            : await getRunningPregameMatch(user, user.userData.puuid);
+        return json<LoaderData>({
+            status: 'pregame',
+        });
+    } catch (e) {
+        if (!(e instanceof NoPregameFoundException)) {
+            throw e;
+        }
+    }
+    try {
+        const mockCoregame = await get('mockCoregame');
+        const coregame = mockCoregame
+            ? true
+            : await getRunningCoregameMatch(user, user.userData.puuid);
+        return json<LoaderData>({
+            status: 'coregame',
+        });
+    } catch (e) {
+        if (!(e instanceof NoCoregameFoundException)) {
+            throw e;
+        }
+    }
+    return json<LoaderData>({
+        status: 'not in game',
+    });
+}
+
+export const loader = async ({ request, params }: DataFunctionArgs) => {
+    const user = await requireUser(request);
+    return await detectGame(user, user.userData.puuid);
+};
